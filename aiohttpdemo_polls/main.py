e38f82dc29a5fc7179b7b8d4b90b4f59837bec9e
Sylvestre Gug <sylvestre.gug@tempus.com>
2019-11-24 14:37:06 -0500

    build: db and rest

--- aiohttpdemo_polls/main.py
+++ aiohttpdemo_polls/main.py
@@ -1,6 +1,12 @@
 from aiohttp import web
-from routes import setup_routes
+
+from aiohttpdemo_polls.db import init_pg, close_pg
+from aiohttpdemo_polls.routes import setup_routes
+from aiohttpdemo_polls.settings import config
 
 app = web.Application()
 setup_routes(app)
+app['config'] = config
+app.on_startup.append(init_pg)
+app.on_cleanup.append(close_pg)
 web.run_app(app)
--- aiohttpdemo_polls/views.py
+++ aiohttpdemo_polls/views.py
@@ -1,4 +1,18 @@
+import datetime
+import json
+
 from aiohttp import web
+from aiohttpdemo_polls import db
+
+def to_serializable(val):
+    if isinstance(val, datetime):
+        return val.isoformat() + "Z"
+    return str(val)
 
 async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+    async with request.app['db'].acquire() as conn:
+        cursor = await conn.execute(db.question.select())
+        records = await cursor.fetchall()
+        questions = [dict(q) for q in records]
+
+        return web.json_response({"data": questions}, dumps=json.dumps)
